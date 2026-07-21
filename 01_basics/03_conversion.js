7fe1d2bfe55148c8217eff5a89c6b523d68cdb8b
Ritaja2122 <ritajamandal236@gmail.com>
2026-07-21 20:36:57 +0000

    conversion types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,14 @@
+let age = "33"
+let valueInNum = Number(age)
+console.log(typeof age);
+
+//"33"->33 string to number
+//"33abc"-> NaN as its a combination of string and number 
+//true -> 1
+// null -> 0
+
+// number to boolean conversion
+// 1 -> true
+//"" -> false
+// "hitesh" -> true
+
