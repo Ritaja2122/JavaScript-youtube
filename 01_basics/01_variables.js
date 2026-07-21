7e96c754f8da307a9a88262ba31b31ff0c4a6b99
Ritaja2122 <ritajamandal236@gmail.com>
2026-07-21 16:57:36 +0000

    variables are tested

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const account_id = 144553
+let accountEmail = "ritman05@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// account_id = 2; it is not allowed as it was declared with const keyword
+accountEmail = "momo@gmail.com"
+accountPassword = "456"
+accountCity = "kolkata"
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
