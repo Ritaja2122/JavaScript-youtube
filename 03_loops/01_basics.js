c50e6c8a19a1d239558fc2c78291371d9931b4fd
Ritaja2122 <ritajamandal236@gmail.com>
2026-07-28 19:14:05 +0000

    loops

---
+++ 03_loops/01_basics.js
@@ -0,0 +1,7 @@
+// for , while , do-while, for-each : all kinda loops
+
+//for
+// kaha se jana hai -> kaha tak jana hai -> kaise jana hai
+
+//while
+//kaha se jana hai -> kab rukna hai -> kaise jana hai
