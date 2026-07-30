8400660422a2523877c4bfb30cc46ea39a426546
Ritaja2122 <ritajamandal236@gmail.com>
2026-07-30 18:01:01 +0000

    functions

---
+++ 04_functions/01_basics.js
@@ -0,0 +1,15 @@
+let func = function(){
+    console.log("this is a function")
+    // function expression
+}
+func()
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function abcd(){
+    console.log("this is function")
+    //function declaration and function statement
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// fat arrow function
+let fnc2 = ()=>{
+
+}
---
+++ 04_functions/02_parameters_and_arguments.js
@@ -0,0 +1,6 @@
+function print(v1){ // v1 is  parameter
+ console.log(`${v1} naach raha hai`)
+}
+print("ghoda");//arguments
+print("mai");
+print("tum");
